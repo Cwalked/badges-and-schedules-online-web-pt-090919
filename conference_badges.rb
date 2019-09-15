c29c051f1be7060b78bc5822a025dd5e593bc834
Courtney Walker <cwalked@gmail.com>
2019-09-15 18:40:55 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -2,3 +2,10 @@ def badge_maker(name)
   name = "Arel"
   "Hello, my name is #{name}."
 end
+
+def batch_badge_creator(array)
+  array = ["Edsger", "Ada", "Charles", "Alan", "Grace", "Linus", "Matz"]
+  array.each do |array|
+    puts "Hello my name is #{array}"
+  end
+end
